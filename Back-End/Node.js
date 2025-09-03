8bb45384014451d21811a71746e03ff3ed8db466
Lenny <50156430@est.ort.edu.ar>
2025-09-03 10:37:39 -0300

    kangseviok

--- Back-End/Node.js
+++ Back-End/Node.js
@@ -1,22 +1,22 @@
 import fs from "fs";
 
+let conectar = JSON.parse(fs.readFileSync("usuario.json", "utf-8"));
+
 function iniciodesesion(usuario,contraseña){
-    const usuarios = [
-        { nombreUsuario: "juan123", contraseña: "1234" },
-        { nombreUsuario: "maria22", contraseña: "abcd" }
+const usuarios = [
+{ nombreUsuario: "juan123", contraseña: "1234" },
+ { nombreUsuario: "maria22", contraseña: "abcd" }
       ];
-    
-      // Buscar si existe el usuario
-      const user = usuarios.find(u => u.nombreUsuario === nombreUsuario);
+    const user = usuarios.find(u => u.nombreUsuario === nombreUsuario);
     
       if (!user) {
-        return "El nombre de usuario no existe ❌";
+        return "El nombre de usuario no existe";
       }
     
       if (user.contraseña === contraseña) {
-        return "Inicio de sesión exitoso ✅";
+        return "Inicio de sesión bien ";
       } else {
-        return "Contraseña incorrecta ❌";
+        return "Contraseña incorrecta ";
       }
 }
 
